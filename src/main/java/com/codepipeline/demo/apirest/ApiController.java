cc0d12770fb43ba4bf44c8faabc2c8c628bfb83a
johnjimenez216 <85650558+johnjimenez216@users.noreply.github.com>
2021-07-14 16:48:29 -0500

    Ajuste rest

--- src/main/java/com/codepipeline/demo/apirest/ApiController.java
+++ src/main/java/com/codepipeline/demo/apirest/ApiController.java
@@ -3,10 +3,11 @@ package com.codepipeline.demo.apirest;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequestMapping("awspipeline")
 @RestController
 public class ApiController {
 	
-	@RequestMapping("/hola")
+	@RequestMapping("/saludo")
 	public String hello(){
 		return "Hola este es un servicio de ejemplo";
 	}
